96b5423d734e83744780c4e8d6d3245dcaf0b319
Its-Isaku <Isai.almeraz@uabc.edu.mx>
2026-02-28 12:28:28 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
